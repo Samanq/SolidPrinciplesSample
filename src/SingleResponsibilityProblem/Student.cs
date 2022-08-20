64bd7f81ee7e3dc6f7553b2e195961649b190b7d
Saman Qaydi (Lap01) <Saman.Qaydi@Gmail.com>
2022-08-20 20:56:03 +0200

    Added student class to SingleResponsibilityProblem

---
+++ src/SingleResponsibilityProblem/Student.cs
@@ -0,0 +1,61 @@
+﻿namespace SingleResponsibilityProblem;
+
+public class Student
+{
+    public string FirstName { get; set; } = string.Empty;
+    public string LastName { get; set; } = string.Empty;
+    public string Email { get; set; } = string.Empty;
+    public string Password { get; set; } = string.Empty;
+    public string VerificationCode { get; set; } = string.Empty;
+
+
+    public void Register()
+    {
+        Console.WriteLine("New student registered.");
+    }
+
+    public void Login(string email, string password)
+    {
+        Console.WriteLine("Student logged in");
+    }
+
+    public void Logout(string email, string password)
+    {
+        Console.WriteLine("Student logged in");
+    }
+
+    public void SendEmailVerificationToken()
+    {
+        Console.WriteLine($"{VerificationCode} has been sent to {Email}");
+    }
+
+    public IEnumerable<Student> GetStudents()
+    {
+        var students = new List<Student>
+        {
+            new Student
+            { 
+                FirstName = "John",
+                LastName = "Doe",
+                Email = "John@Domain.com",
+                Password = "1234"
+            },
+            new Student
+            {
+                FirstName = "Jane",
+                LastName = "Doe",
+                Email = "Jane@Domain.com",
+                Password = "4321"
+            },
+            new Student
+            {
+                FirstName = "Peter",
+                LastName = "Jackson",
+                Email = "Peter@Domain.com",
+                Password = "12345"
+            },
+        };
+
+        return students;
+    }
+}
